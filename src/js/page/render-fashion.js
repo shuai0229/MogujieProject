6d594d2011a651432d8a60eab859b510107661b3
zhangshuai <1226791563@qq.com>
2017-06-07 19:16:55 +0800

    zhangshuai

---
+++ src/js/page/render-fashion.js
@@ -0,0 +1,19 @@
+//var $ = require("../lib/jquery-2.1.1.min.js");
+$(function() {
+	var page = {
+		fashionInit: function() {
+			this.fashow();
+			this.faTab();
+		},
+		fashow: function() {
+			$(".fashion-main").eq(0).show();
+		},
+		faTab: function() {
+			$(".colligate-tab").on("click", ".tab", function() {
+				$(this).addClass("active").siblings().removeClass("active");
+				$(".fashion-main").eq($(this).index()).show().siblings(".fashion-main").hide();
+			})
+		}
+	}
+	page.fashionInit();
+})
--- src/js/release/index.js
+++ src/js/release/index.js
@@ -3,12 +3,13 @@
  */
 require('../../css/lib/reset.css');
 require('../../css/lib/swiper.min.css');
-require('../../css/page/style-index.css');//首页
-require('../../css/page/style-classify.css');//分类
-require('../../css/page/style-shoppingCart.css');//购物车
-require('../../css/page/style-mine.css');//我的
-require('../../css/page/style-share.css');//分享
-require('../../css/page/style-details.css');//详情
+require('../../css/page/style-index.css'); //首页
+require('../../css/page/style-classify.css'); //分类
+require('../../css/page/style-shoppingCart.css'); //购物车
+require('../../css/page/style-mine.css'); //我的
+require('../../css/page/style-share.css'); //分享
+require('../../css/page/style-details.css'); //详情
+require('../../css/page/style-fashion.css'); //时尚
 require('../lib/jquery-2.1.1.min');
 require('../lib/vue.min');
 require('../lib/swiper.min');
@@ -19,3 +20,4 @@ require('../page/render-shoppingCart');
 require('../page/render-mine');
 require('../page/render-share');
 require('../page/render-details');
+require('../page/render-fashion');
